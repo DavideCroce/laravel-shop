1bbe4936d46905d056dc8e2e3933d31a08c373ad
IskahnX <davidecroce10@gmail.com>
2022-03-24 17:06:17 +0100

    add css home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.main')
 @section('content')
-    <div class="container d-flex">
-        <div class="welcome">
-            <h1>Welcome to the 52 shop!!</h1>
+    <div class="d-flex flex-column homepage">
+        <div class="home-page-text">
+            <div class="welcome">
+                <h1>Welcome to the 52 shop!!</h1>
+            </div>
+            <p>We're the first shop in this country.
+                We sell everything you can think of (and obviously much more!)
+            </p>
+            <h4>What are you waiting?? Go and dive into the class 52 shop!!!</h4>
         </div>
-        <p>We're the first shop in this country.
-            We sell everything you can think of (and obviously much more!)
-        </p>
-        <h4>What are you waiting?? Go and dive into the class 52 shop!!!</h4>
     </div>
 @endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.main')
 
 @section('content')
-<ul>
-    @foreach ($products as $product)
-    <li>
-        <figure>
-            <img src="{{ $product->image }}" alt="">
-        </figure>
-        <h2>{{ $product->name }}</h2>
-        <p>{{ $product->description }}</p>
-        <div>{{ $product->price }}</div>
-        <a href="{{ route('products.show', ['product' => $product->id]) }}">Dettagli</a>
-    </li>
-    @endforeach
-</ul>
+    <ul>
+        @foreach ($products as $product)
+            <li>
+                <figure>
+                    <img src="{{ $product->image }}" alt="">
+                </figure>
+                <h2>{{ $product->name }}</h2>
+                <p>{{ $product->description }}</p>
+                <div>{{ $product->price }}</div>
+                <a href="{{ route('products.show', ['product' => $product->id]) }}">Dettagli</a>
+            </li>
+        @endforeach
+    </ul>
+    <a href="{{ route('products.create') }}" class="btn btn-primary"> Add a new product!</a>
 @endsection
